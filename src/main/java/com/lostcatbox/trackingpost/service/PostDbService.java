1cb31bec10f03095ad73f1f973e887ee607e8d31
lostcatbox <lostcatbox@gmail.com>
2022-09-02 13:11:46 +0900

    addedPostDBservice

--- src/main/java/com/lostcatbox/trackingpost/service/PostDbService.java
+++ src/main/java/com/lostcatbox/trackingpost/service/PostDbService.java
@@ -1,2 +1,19 @@
-package com.lostcatbox.trackingpost.service;public class PostDbService {
+package com.lostcatbox.trackingpost.service;
+
+import com.lostcatbox.trackingpost.domain.PostDto;
+import com.lostcatbox.trackingpost.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Check;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostDbService {
+    private final PostRepository postRepository;
+
+    //save
+    public PostDto savePost(PostDto postDto){
+        postRepository.save(postDto.toEntity());
+        return postDto;
+    }
 }
