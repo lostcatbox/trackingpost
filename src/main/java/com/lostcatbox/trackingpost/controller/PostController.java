4d9a7191ed930678634c5a3a177b00314115f8d3
lostcatbox <lostcatbox@gmail.com>
2022-09-06 19:25:28 +0900

    add_Service_validRequest

--- src/main/java/com/lostcatbox/trackingpost/controller/PostController.java
+++ src/main/java/com/lostcatbox/trackingpost/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.lostcatbox.trackingpost.controller;
 
 import com.lostcatbox.trackingpost.domain.PostDto;
+import com.lostcatbox.trackingpost.domain.RequestInfo;
+import com.lostcatbox.trackingpost.service.KindRequest;
 import com.lostcatbox.trackingpost.service.PostDbService;
 import com.lostcatbox.trackingpost.service.PostManager;
+import com.lostcatbox.trackingpost.service.ValidRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,12 @@ public class PostController {
     PostManager postManager;
     @Autowired
     PostDbService postDbService;
+    @Autowired
+    ValidRequest validRequest;
     @GetMapping (value = "/")
     public PostDto gethomepage(){
-        PostDto postDto = postManager.getpost();
+        RequestInfo requestInfo = validRequest.getinfo(); // 요청에 대한 정보 추출
+        PostDto postDto = postManager.getpost(requestInfo); //해당 요청에 대해 post 정보 추출
         postDbService.savePost(postDto); //dbtest, 추후 다른 msa로 빠질것임
         return postDto;
     }
