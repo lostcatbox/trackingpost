7df65d73ca0f5f8967e433cdd2fcaaebf38d2a13
lostcatbox <lostcatbox@gmail.com>
2022-09-15 10:12:07 +0900

    postcompanyenumValueAdd

--- trackingpost-core/src/main/java/com/example/trackingpostcore/domain/PostCompanyEnum.java
+++ trackingpost-core/src/main/java/com/example/trackingpostcore/domain/PostCompanyEnum.java
@@ -1,5 +1,20 @@
 package com.example.trackingpostcore.domain;
 
 public enum PostCompanyEnum {
-    CJ,CU
+    CJ("CJ대한통운"),
+    CU("CU편의점택배"),
+    EPOST("우체국택배"),
+    HANJIN("한진택배"),
+    LOTTE("롯데택배"),
+    EMS("EMS"),
+    ILOGEN("로젠택배"),
+    CVSNET("CVSNet"),
+    DHL("DHL");
+    private final String value;
+    PostCompanyEnum(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
 }
