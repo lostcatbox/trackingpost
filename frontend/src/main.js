d53e1db0c800da91165bb225744a2f5ec0bc06f9
lostcatbox <lostcatbox@gmail.com>
2022-09-19 16:31:18 +0900

    homeserviceAPIUrlChangoToMyServerUri

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,5 +15,5 @@ app
     .mount('#app')
 
 app.config.globalProperties.$axios = axios;  //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
-app.config.globalProperties.$homeserviceAPIUrl = 'http://localhost:8080' //homeservice api server
+app.config.globalProperties.$homeserviceAPIUrl = 'http://tracking.loscatbox.com:8080' //homeservice api server
 
