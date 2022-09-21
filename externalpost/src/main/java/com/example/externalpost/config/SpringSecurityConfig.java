d9bd2417635f1e18202bfe9e50d0e16086537d6a
lostcatbox <lostcatbox@gmail.com>
2022-09-21 15:02:32 +0900

    postcontrollerUpdateWithOptional

--- externalpost/src/main/java/com/example/externalpost/config/SpringSecurityConfig.java
+++ externalpost/src/main/java/com/example/externalpost/config/SpringSecurityConfig.java
@@ -1,16 +1,2 @@
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SpringSecurityConfig  extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .anyRequest().permitAll();
-    }
+package com.example.externalpost.config;public class SpringSecurityConfig {
 }
