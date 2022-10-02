cd18a3a8617535a1587967449ff14fe6f901b125
lostcatbox <lostcatbox@gmail.com>
2022-10-02 13:30:26 +0900

    UpdateServicehome(OptionalAndAdvice)

--- servicehome/src/main/java/com/lostcatbox/trackingpost/controller/PostController.java
+++ servicehome/src/main/java/com/lostcatbox/trackingpost/controller/PostController.java
@@ -30,33 +30,22 @@ public class PostController {
     // id값은 DB아이디 값이며, kakaoid값으로 추후 바뀔수있다. 하지만 현재로는 이렇게 검증하고, 가장 최근 자료를 보여주는것이 좋은듯
     @GetMapping ("/{userId}/{postNumber}/")
     public ResponsePost getpost(@PathVariable String userId,@PathVariable String postNumber){
-        Optional<PostDto> recentPost = postDbService.getRecentPost(userId, postNumber);//인자 &조건으로 가장최근 데이터 반환, 없다면 null
-        return new ResponsePost(recentPost.orElseGet(null));
+        PostDto recentPost = postDbService.getRecentPost(userId, postNumber);//인자 &조건으로 가장최근 데이터 반환, 없다면 null
+        return new ResponsePost(recentPost);
     }
      //내 서버로 응답한 link경로로 접근한후 post로 새로고침 명령시 -> kafka로 외부 택배 조회 메세지 보내기 pub
     @PostMapping("/{userId}/{postNumber}/")
     public HttpStatus requestRefresh(@PathVariable String userId, @PathVariable String postNumber, @RequestParam String postCompany){
         PostCompanyEnum postCompanyEnum = PostCompanyEnum.valueOfName(postCompany);
-        if (ObjectUtils.isEmpty(postCompanyEnum)){
-            return HttpStatus.NOT_FOUND;
-        }
-        else{
-            //바디체크후에 택배회사 가져오는 로직 필요함. -> 그리고 아래 로직에 해당 택배회사 Enum값으로 교체하는것필요 -> 실패시 404에러
-            RequestInfo requestInfo = new RequestInfo(postNumber,userId, postCompanyEnum);
-            kafkaTemplate.send(topicName,requestInfo);
-            return HttpStatus.OK;
-        }
+        RequestInfo requestInfo = new RequestInfo(postNumber,userId, postCompanyEnum);
+        kafkaTemplate.send(topicName,requestInfo);
+        return HttpStatus.OK;
     }
     //해당유저의 모든 택배조회 기록 반환
     @GetMapping("/{userId}/")
     public List<ResponsePost> getpostlist(@PathVariable String userId){
         List<PostDto> postList = postDbService.getPostList(userId);
-        if (ObjectUtils.isEmpty(postList)){ //Object의 null값을 체크한다!
-            return new ArrayList<>();
-        }
-        else{
-            return convert(postList);
-        }
+        return convert(postList);
     }
     private List<ResponsePost> convert(List<PostDto> postList) {
         List<ResponsePost> responsePostList=new ArrayList<>();
