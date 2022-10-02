4d675e2b051451a939508573dabf32b6a8499cdf
lostcatbox <lostcatbox@gmail.com>
2022-10-02 13:29:57 +0900

    kakaochannelUpdate(OptionalAndAdvice)

--- kakaochannel/src/main/java/com/example/kakaochannel/controller/KakaoController.java
+++ kakaochannel/src/main/java/com/example/kakaochannel/controller/KakaoController.java
@@ -1,19 +1,17 @@
 package com.example.kakaochannel.controller;
 
 import com.example.kakaochannel.domain.KakaoLinkResponse;
+import com.example.kakaochannel.service.ResponseService;
 import com.example.trackingpostcore.domain.RequestInfo;
 import com.example.kakaochannel.service.ValidRequest;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
+
+import static javax.swing.UIManager.getString;
 
 @RestController
 @Slf4j
@@ -21,30 +19,21 @@ public class KakaoController {
     @Autowired
     ValidRequest validRequest;
     @Autowired
+    ResponseService responseService;
+    @Autowired
     private KafkaTemplate<String, RequestInfo> kafkaTemplate;
 
     private static final String topicName = "posttopic";
 
     @PostMapping(value = "/")
-    public String gethomepage(@RequestBody String params){
-        Optional<RequestInfo> requestInfo = validRequest.getinfo(params); // 요청에 대한 정보 추출
-        JsonObject obj = new JsonObject();
-        obj.addProperty("version","2.0");
-        JsonObject data = new JsonObject();
-
-        requestInfo.ifPresentOrElse((r)-> {
-            KakaoLinkResponse linkResponse = new KakaoLinkResponse(r); //해당 링크 작성된 DTO// 지역변수여야함.
-            kafkaTemplate.send(topicName, r);
-            data.addProperty("postDataLink", linkResponse.getPostDataLink());
-            data.addProperty("requestUser", linkResponse.getRequestUser());
-            data.addProperty("postCompany", linkResponse.getPostCompany().getName());
-            data.addProperty("postNumber", linkResponse.getPostNumber());
-        }, ()->{
-            data.addProperty("postDataLink", "네트워크 오류 혹은 지원하지 않는 택배사");
-                });
-
-        obj.add("data", data);
-
-        return obj.toString();
+    public String gethomepage(@RequestBody String params) {
+        RequestInfo requestInfo = validRequest.getinfo(params); // 요청에 대한 정보 추출
+        String result = responseService.getFailResponse(); ////반드시 카카오엔 응답해야하는상황.서비스로빼고, advice에도 Exception후 return 으로 getFailResponse해도되는지
+        if (requestInfo != null) { //null체크? catch하지못한오류? 혹시모르는오류?
+            result = responseService.getSuccessResponse(requestInfo);
+            kafkaTemplate.send(topicName, requestInfo);
+        }
+        return result;
     }
 }
+
--- kakaochannel/src/main/java/com/example/kakaochannel/service/ValidRequest.java
+++ kakaochannel/src/main/java/com/example/kakaochannel/service/ValidRequest.java
@@ -1,5 +1,8 @@
 package com.example.kakaochannel.service;
 
+import com.example.kakaochannel.advice.exception.RequestValidIOCException;
+import com.example.kakaochannel.advice.exception.RequestValidIllegalCException;
+import com.example.kakaochannel.advice.exception.RequestValidNullCException;
 import com.example.trackingpostcore.domain.RequestInfo;
 import com.example.trackingpostcore.domain.PostCompanyEnum;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -20,7 +23,7 @@ import java.util.Optional;
 @Service
 @Slf4j
 public class ValidRequest { //추후에 데이터 받아서 정보 추출하는 역할 하는클래스
-    public Optional<RequestInfo> getinfo(String params){
+    public RequestInfo getinfo(String params){
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> resultMap = new HashMap<>();
         try {
@@ -44,19 +47,17 @@ public class ValidRequest { //추후에 데이터 받아서 정보 추출하는 
             } else {
                 requestInfo.setRequestUser(requestuser.substring(0, 20)); //product기준은 requestuser 값 너무큼. 20자로 자름
             }
-            return Optional.ofNullable(requestInfo);
+            return requestInfo;
         }
         catch (IllegalArgumentException e){
-            e.printStackTrace();
-            return Optional.empty(); //"null에러시 반환"
+            throw new RequestValidIllegalCException("kakao채널 요청 valid중  IllegalArgumentException 발생 \n자세히: "+e.getMessage());
         }
         catch (NullPointerException e){
-            e.printStackTrace();
-            return Optional.empty(); //"null에러시 반환"
+            throw new RequestValidNullCException("kakao채널 요청 valid중 NPE발생 자세히: "+e.getMessage());
         }
         catch (IOException e){
             e.printStackTrace();
-            return Optional.empty(); // "null에러시 반환"
+            throw new RequestValidIOCException("kakao채널 요청 valid중 IOException발생 자세히: " + e.getMessage());
         }
     }
 }
